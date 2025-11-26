1323f54e0fd0fa07b3a311a48dea190ac633c8a3
Pablo Eduardo <pabloeduardossantos@gmail.com>
2025-11-26 10:53:25 -0300

    chore: adicionado singleton e mock

---
+++ api/__mock__/singleton/prisma-singleton.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { PrismaClient } from 'generated/prisma/client';
+import { DeepMockProxy, mockDeep, mockReset } from 'jest-mock-extended';
+import PrismaSingleton from 'src/singleton/prisma-singleton';
+
+jest.mock('../singletons/prisma-singleton/prisma-singleton', () => {
+  return {
+    __esModule: true,
+    default: {
+      instance: { client: mockDeep<PrismaClient>() },
+    },
+  };
+});
+
+beforeEach(() => {
+  mockReset(prismaMock);
+});
+
+export const prismaMock = PrismaSingleton.instance
+  .client as unknown as DeepMockProxy<PrismaClient>;
---
+++ api/src/singleton/prisma-singleton.ts
@@ -0,0 +1,32 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'generated/prisma/client';
+import { env } from 'node:process';
+
+export default class PrismaSingleton {
+  static #instance: PrismaSingleton;
+  #prisma: PrismaClient;
+
+  private constructor() {
+    const connectionString: string = `${env.DATABASE_URL}`;
+    const adapter: PrismaPg = new PrismaPg({ connectionString });
+    this.#prisma = new PrismaClient({
+      adapter,
+      log: ['query', 'info', 'warn', 'error'],
+    });
+  }
+
+  public static get instance(): PrismaSingleton {
+    if (!PrismaSingleton.#instance) {
+      PrismaSingleton.#instance = new PrismaSingleton();
+    }
+    return PrismaSingleton.#instance;
+  }
+
+  public get client(): PrismaClient {
+    return this.#prisma;
+  }
+
+  async disconnect(): Promise<void> {
+    await this.#prisma.$disconnect();
+  }
+}
