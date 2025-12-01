aa6cc89718f439af9d8ccf6868057b52529b9468
Pablo Eduardo <pabloeduardossantos@gmail.com>
2025-12-01 11:56:59 -0300

    chore: implementar interação do text-field ao angular forms

--- web/src/app/features/auth/login/login.ts
+++ web/src/app/features/auth/login/login.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { TextField } from '../../../shared/components/inputs/text-field/text-field';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [TextField],
+  imports: [TextField, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({ cpf: ['cpf', Validators.minLength(4)] });
+  }
+}
--- web/src/app/shared/components/inputs/text-field/text-field.ts
+++ web/src/app/shared/components/inputs/text-field/text-field.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, effect, forwardRef, input, InputSignal } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const TEXT_FIELD_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => TextField),
+  multi: true,
+};
 
 @Component({
   selector: 'app-text-field',
-  imports: [],
+  imports: [FormsModule],
+  providers: [TEXT_FIELD_ACCESSOR],
   templateUrl: './text-field.html',
   styleUrl: './text-field.css',
 })
-export class TextField {
+export class TextField implements ControlValueAccessor {
+  //Inputs
+  value: InputSignal<string> = input<string>('');
+  label: InputSignal<string> = input<string>('Label');
+  hasLabel: InputSignal<boolean> = input<boolean>(false);
+  placeholder: InputSignal<string> = input<string>('');
 
+  //Values Internos
+  innerValue: string | Record<string, any> = '';
+  private usingForms = false;
+
+  constructor() {
+    effect(() => {
+      if (!this.usingForms) {
+        this.innerValue = this.value();
+      }
+    });
+  }
+
+  // Funções do CVA
+  onChange: (value: string) => void = () => {};
+  onTouched: () => void = () => {};
+
+  writeValue(obj: any): void {
+    this.usingForms = true;
+    this.innerValue = obj;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {}
+
+  onBlur(): void {
+    this.onTouched();
+  }
+
+  updateValue(event: any) {
+    const newValue = event.target.value;
+    this.innerValue = newValue;
+    if (this.usingForms) {
+      this.onChange(newValue);
+    }
+  }
 }
