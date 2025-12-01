208f915117f086649c52fcfe7b876171713eda4e
heyitsmepablo <pabloeduardossantos@gmail.com>
2025-11-30 21:54:55 -0300

    chore: criar text-field-component

--- web/src/app/features/auth/login/login.ts
+++ web/src/app/features/auth/login/login.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TextField } from '../../../shared/components/inputs/text-field/text-field';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [TextField],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
-
-}
+export class Login {}
