4ef62e0c299468ba10df4d69607297efb1444527
Pablo Eduardo <pabloeduardossantos@gmail.com>
2025-12-04 17:32:02 -0300

    chore(api): adicionando validação no login-form

--- web/src/app/features/auth/components/login-form/login-form.ts
+++ web/src/app/features/auth/components/login-form/login-form.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { TextField } from '../../../../shared/components/inputs/text-field/text-field';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
-  imports: [TextField],
+  imports: [TextField, ReactiveFormsModule],
   templateUrl: './login-form.html',
   styleUrl: './login-form.css',
 })
-export class LoginForm {}
+export class LoginForm {
+  loginForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      cpf: ['', Validators.minLength(11)],
+      password: [''],
+    });
+  }
+
+  onSubmit(): void {}
+}
