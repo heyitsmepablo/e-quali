fa6e917b2352e9cd0f3bad49d0ab4d1e9d74578f
Pablo Eduardo <pabloeduardossantos@gmail.com>
2025-12-04 12:06:39 -0300

    chore: começando a reset password

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/pages/login/login';
+import { ResetPassword } from './features/auth/pages/reset-password/reset-password';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: Login },
+  {path: 'reset-password', component:ResetPassword}
 ];
